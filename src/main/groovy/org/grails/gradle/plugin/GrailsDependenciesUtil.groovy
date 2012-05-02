35496ce511a5b369476a10df99d7bb687132f98c
graemerocher <graeme.rocher@gmail.com>
2012-05-02 15:17:14 +0200

    fix dependency handling for Grails 2.0

--- src/main/groovy/org/grails/gradle/plugin/GrailsDependenciesUtil.groovy
+++ src/main/groovy/org/grails/gradle/plugin/GrailsDependenciesUtil.groovy
@@ -8,18 +8,27 @@ class GrailsDependenciesUtil {
         String name = configuration.name
 
         project.dependencies {
-            ["bootstrap", "scripts", "resources"].each {
-                "$name"("org.grails:grails-$it:${grailsVersion}") {
-                    exclude group: "org.slf4j"
+            if(grailsVersion.startsWith("1.")) {
+                ["bootstrap", "scripts", "resources"].each {
+                    "$name"("org.grails:grails-$it:${grailsVersion}") {
+                        exclude group: "org.slf4j"
+                    }
                 }
-            }
 
-            if (grailsVersion.startsWith("1.3")) {
-                "$name"("org.apache.ant:ant:1.8.2")
-            }
+                if (grailsVersion.startsWith("1.3")) {
+                    "$name"("org.apache.ant:ant:1.8.2")
+                }
 
-            if (!grailsVersion.startsWith("2")) {
-                "$name"("org.apache.ivy:ivy:2.1.0")
+                if (!grailsVersion.startsWith("2")) {
+                    "$name"("org.apache.ivy:ivy:2.1.0")
+                }
+
+            }
+            else {
+                "$name"("org.grails:grails-dependencies:${grailsVersion}")
+                "$name"("org.grails:grails-scripts:${grailsVersion}")
+                "$name"("org.grails:grails-resources:${grailsVersion}")
+
             }
         }
     }
