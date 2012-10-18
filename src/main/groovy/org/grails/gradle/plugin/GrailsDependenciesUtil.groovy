0bb5ea8e3b31ecd8cb8a2f7dd734baef1365965a
Luke Daley <ld@ldaley.com>
2012-10-18 09:01:35 -0400

    reformat.

--- src/main/groovy/org/grails/gradle/plugin/GrailsDependenciesUtil.groovy
+++ src/main/groovy/org/grails/gradle/plugin/GrailsDependenciesUtil.groovy
@@ -1,14 +1,15 @@
 package org.grails.gradle.plugin
 
-import org.gradle.api.artifacts.Configuration
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 
 class GrailsDependenciesUtil {
+
     static configureBootstrapClasspath(Project project, String grailsVersion, Configuration configuration) {
         String name = configuration.name
 
         project.dependencies {
-            if(grailsVersion.startsWith("1.")) {
+            if (grailsVersion.startsWith("1.")) {
                 ["bootstrap", "scripts", "resources"].each {
                     "$name"("org.grails:grails-$it:${grailsVersion}") {
                         exclude group: "org.slf4j"
@@ -22,14 +23,12 @@ class GrailsDependenciesUtil {
                 if (!grailsVersion.startsWith("2")) {
                     "$name"("org.apache.ivy:ivy:2.1.0")
                 }
-
-            }
-            else {
+            } else {
                 "$name"("org.grails:grails-dependencies:${grailsVersion}")
                 "$name"("org.grails:grails-scripts:${grailsVersion}")
                 "$name"("org.grails:grails-resources:${grailsVersion}")
-
             }
         }
     }
+
 }
