df36c261777998d77ecdf4d24a06e530550f65ad
Luke Daley <ld@ldaley.com>
2012-10-18 09:36:05 -0400

    Copyright headers

--- src/test/groovy/org/grails/gradle/plugin/integ/IntegSpec.groovy
+++ src/test/groovy/org/grails/gradle/plugin/integ/IntegSpec.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.grails.gradle.plugin.integ
 
 import org.gradle.GradleLauncher
@@ -8,7 +24,6 @@ import org.gradle.api.tasks.TaskState
 import org.junit.Rule
 import org.junit.rules.TemporaryFolder
 import spock.lang.Specification
-import org.grails.gradle.plugin.GrailsPlugin
 
 abstract class IntegSpec extends Specification {
     @Rule final TemporaryFolder dir = new TemporaryFolder()
@@ -45,7 +60,7 @@ abstract class IntegSpec extends Specification {
     File file(String path) {
         def parts = path.split("/")
         if (parts.size() > 1) {
-            dir.newFolder(*parts[0..-2])
+            dir.newFolder(* parts[0..-2])
         }
         dir.newFile(path)
     }
