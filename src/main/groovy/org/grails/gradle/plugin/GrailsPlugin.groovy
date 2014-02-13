c446fb21d588e76205bcfaf467d1f51020b08408
John Engelman <john.r.engelman@gmail.com>
2014-02-13 12:54:32 -0600

    Only add springloaded agent for run-app task

--- src/main/groovy/org/grails/gradle/plugin/GrailsPlugin.groovy
+++ src/main/groovy/org/grails/gradle/plugin/GrailsPlugin.groovy
@@ -120,6 +120,21 @@ class GrailsPlugin implements Plugin<Project> {
                 map("testClasspath") { testConfiguration }
                 map("sourceSets") { grailsProject.sourceSets }
 
+                map("springloaded") { null }
+            }
+
+            doFirst {
+                if (grailsProject.grailsVersion == null) {
+                    throw new InvalidUserDataException("You must set 'grails.grailsVersion' property before Grails tasks can be run")
+                }
+            }
+        }
+
+        //Only add springloaded to the run-app task
+        //This is the same thing that grailsStart does so we should do it too.
+        project.tasks.getByName(GrailsTaskConfigurator.GRAILS_RUN_TASK) { GrailsTask task ->
+            ConventionMapping conventionMapping = task.conventionMapping
+            conventionMapping.with {
                 map("springloaded") {
                     if (springloadedConfiguration.dependencies.empty) {
                         def defaultSpringloaded = project.dependencies.create("org.springsource.springloaded:springloaded-core:$grailsProject.springLoadedVersion")
@@ -134,12 +149,6 @@ class GrailsPlugin implements Plugin<Project> {
                     } else {
                         springloadedConfiguration
                     }
-                }
-            }
-
-            doFirst {
-                if (grailsProject.grailsVersion == null) {
-                    throw new InvalidUserDataException("You must set 'grails.grailsVersion' property before Grails tasks can be run")
                 }
             }
         }
